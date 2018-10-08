eb4b087d04e5277299ed752be35121454b9bf4ef
Anthony Hollis <4053071+FocusedBit@users.noreply.github.com>
2018-10-08 07:15:47 -0400

    added Php form

---
+++ Sandbox/Php Test/process.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Forms</title>
+    </head>
+    <body>
+        <section>
+            <p>
+                <?php
+                    print('The value is: '.$_GET['val']);
+                ?>
+            </p>
+
+        </section>
+    </body>
+</html>
